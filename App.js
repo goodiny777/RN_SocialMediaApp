5997132304f44d429253999c41396b3067f2487f
Misha <goodiny777@gmial.com>
2024-08-26 03:48:45 -0600

    -change to js template -added custom font Inter -added react-native-asset

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+
+  return <SafeAreaView>
+    <Text style={{fontFamily: 'Inter-Black', fontSize: 50}}>HELLO</Text>
+  </SafeAreaView>;
+}
+
+
+export default App;
