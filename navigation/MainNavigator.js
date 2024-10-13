05fcc7b2572b64e8f7bae47fec1a13b986a2e391
Misha <goodiny777@gmial.com>
2024-10-13 06:03:59 -0600

    added tabs navigator to profile page

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -4,9 +4,19 @@ import Home from "../screens/Home/Home";
 import { Header } from "react-native/Libraries/NewAppScreen";
 import Profile from "../screens/Profile/Profile";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+const ProfileTabs = createMaterialTopTabNavigator();
+
+export const ProfileTabsavigator = () => {
+    <ProfileTabs.Navigator>
+        <ProfileTabs.Screen name={'Tab1'} component={Home} />
+        <ProfileTabs.Screen name={'Tab2'} component={Home} />
+        <ProfileTabs.Screen name={'Tab3'} component={Home} />
+    </ProfileTabs.Navigator>
+};
 
 const DrawerNavigator = () => {
     return (
