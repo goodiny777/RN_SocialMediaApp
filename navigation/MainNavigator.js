b937dc795aa38388f464cd77d3572fb2f31fed03
Misha <goodiny777@gmial.com>
2024-11-08 15:08:32 -0700

    Profile tabs improvement

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -5,18 +5,30 @@ import { Header } from "react-native/Libraries/NewAppScreen";
 import Profile from "../screens/Profile/Profile";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import TabTitle from "../components/TabTitle/TabTitle";
+import { TabBarIndicator } from "react-native-tab-view";
+import { title } from "process";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 const ProfileTabs = createMaterialTopTabNavigator();
 
-export const ProfileTabsavigator = () => {
-    <ProfileTabs.Navigator>
-        <ProfileTabs.Screen name={'Tab1'} component={Home} />
-        <ProfileTabs.Screen name={'Tab2'} component={Home} />
-        <ProfileTabs.Screen name={'Tab3'} component={Home} />
-    </ProfileTabs.Navigator>
+export const ProfileTabsNavigator = () => {
+    return (<ProfileTabs.Navigator screenOptions={{
+        tabBarIndicatorStyle: {
+            backgroundColor: 'transparent',
+        },
+        tabBarStyle: {
+            zIndex: 0,
+        }
+    }}>
+        <ProfileTabs.Screen options={{ tabBarLabel: ({ focused }) => TabTitle({ title: 'Photos', isFocused: focused }) }} name={'Photos'} component={Home} />
+        <ProfileTabs.Screen options={{ tabBarLabel: ({ focused }) => TabTitle({ title: 'Videos', isFocused: focused }) }} name={'Videos'} component={Home} />
+        <ProfileTabs.Screen options={{ tabBarLabel: ({ focused }) => TabTitle({ title: 'Saved', isFocused: focused }) }} name={'Saved'} component={Home} />
+    </ProfileTabs.Navigator>);
 };
+
+
 
 const DrawerNavigator = () => {
     return (
@@ -26,7 +38,7 @@ const DrawerNavigator = () => {
             <Stack.Screen name={Routes.Home} component={Home} />
             <Stack.Screen name={Routes.Profile} component={Profile} />
         </Drawer.Navigator>
-    )
+    );
 };
 
 const MainNavigator = () => {
